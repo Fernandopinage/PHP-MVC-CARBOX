3ff26b489b7497ffee68894192672a3bec273894
FernandoPinage <luizfernandoluck@hotmail.com>
2021-07-04 15:07:32 -0400

    atualização

--- app/dao/CompradorDAO.php
+++ app/dao/CompradorDAO.php
@@ -42,7 +42,7 @@ class CompradorDAO extends DAO{
                 'id' => $row['COMPRADOR_ID'],
                 'nome' => $row['COMPRADOR_NOME'],
                 'email' => $row['COMPRADOR_EMAIL'],
-                'status' => $row['COMPRADOR_STATUS']
+                'status' => 'N'
             );
             header('Location: ../php/home.php?p=home/');
             
