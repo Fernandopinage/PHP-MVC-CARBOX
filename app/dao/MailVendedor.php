afc205d5f1575761ce6009a385b70eca18db6a90
FernandoPinage <luizfernandoluck@hotmail.com>
2021-08-03 10:37:56 -0400

    atualizando Carboxi

--- app/dao/MailVendedor.php
+++ app/dao/MailVendedor.php
@@ -71,7 +71,7 @@ class VendedorMAIL
                             <div>
                                 <h1 class="font-weight-light" style="font-weight:Arial;">Seja bem-vindo ao Portal de Vendas da CARBOXI.</h1>
                                 <h3 class="font-weight-light">Olá, <b style="color:#136132;">' . $nome . '</b> você foi cadastrado no sistema de compras da Carboxi.</h3>
-                                <h3 class="font-weight-light">Para efetuar o primeiro acesso  <a href="https://carboxsistema.agenciaprogride.com.br/app/php/acesso.php?email='.$email.'&senha='.$rash.'"> click no link </a> para definir uma senha.</h3>
+                                <h3 class="font-weight-light">Para efetuar o primeiro acesso  <a href="https://carboxigases.com/carboxi_sistema/app/php/acesso.php?email='.$email.'&senha='.$rash.'"> click no link </a> para definir uma senha.</h3>
                             </div>
                         </div>
                     </div>
